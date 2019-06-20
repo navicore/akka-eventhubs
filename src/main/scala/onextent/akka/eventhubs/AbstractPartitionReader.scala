0893b3ab32b155e0d3548bf575e7a57b8ad55255
Ed Sweeney <ed@onextent.com>
2019-06-20 08:42:02 -0700

    change to future processing but msft is not propogating errors

--- src/main/scala/onextent/akka/eventhubs/AbstractPartitionReader.scala
+++ src/main/scala/onextent/akka/eventhubs/AbstractPartitionReader.scala
@@ -1,12 +1,16 @@
 package onextent.akka.eventhubs
 
+import java.lang
 import java.time.Duration
-import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.{CompletableFuture, ScheduledExecutorService}
 
 import akka.actor.Actor
 import com.microsoft.azure.eventhubs._
 import com.typesafe.scalalogging.LazyLogging
 import onextent.akka.eventhubs.Connector.Event
+
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 abstract class AbstractPartitionReader(partitionId: Int,
                                        eventHubConf: EventHubConf)
@@ -37,24 +41,36 @@ abstract class AbstractPartitionReader(partitionId: Int,
   def initReceiver: () => Unit = () => {
     receiver.setReceiveTimeout(Duration.ofSeconds(20))
   }
-  // wheel to call from init
+
   def read(): List[Event] = {
     var result: List[EventData] = List()
     while (result.isEmpty) {
-      val receivedEventsOpt = Option(receiver.receiveSync(ehRecieverBatchSize))
-      result = receivedEventsOpt match {
-        case Some(receivedEvents) =>
-          val itero = Option(receivedEvents.iterator())
+      import scala.compat.java8.FutureConverters._
+      import scala.concurrent.duration._
+      val cs: CompletableFuture[lang.Iterable[EventData]] =
+        receiver.receive(ehRecieverBatchSize)
+      val sf: Future[lang.Iterable[EventData]] = toScala(cs)
+      implicit def ec: ExecutionContext = ExecutionContext.global
+      val tryresult: Try[lang.Iterable[EventData]] =
+        Await.ready(sf, 60.seconds).value.get
+      result = tryresult match {
+        case Success(r) if r != null=>
+          val itero = Option(r.iterator())
           itero match {
             case Some(iter) if iter.hasNext =>
               import scala.collection.JavaConverters._
               val r: List[EventData] = iter.asScala.toList
               logger.debug(
-                s"read ${r.length} messages with read batch size of $ehRecieverBatchSize")
+                s"read ${r.length} java future messages with read batch size of $ehRecieverBatchSize")
               r
             case _ => List()
           }
-        case _ => List()
+        case Success(_) =>
+          logger.warn(s"read failed - null result")
+          throw new java.io.IOException("read failed - null result")
+        case Failure(e) =>
+          logger.error(s"read failed due to $e", e)
+          throw e
       }
     }
     result.map(eventData => Event(self, partitionId, eventData))
